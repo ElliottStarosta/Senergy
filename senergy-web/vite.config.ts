158663ddd44c0f9f4bc9eedd2870d789cc9af8ab
Elliott <starlliott@gmail.com>
2025-12-29 10:22:42 -0500

    changed config file to make /api-docs work
    /api/ do make it resolve route errors

--- senergy-web/vite.config.ts
+++ senergy-web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     host: '127.0.0.1',
     proxy: {
-      '/api': {
+      '/api/': {
         target: 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
